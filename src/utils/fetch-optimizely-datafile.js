07aebe12a5c98ffb4f3882ff7e02d25d53829862
Marshall Serlin <marshallserlin@gmail.com>
2023-07-28 12:53:18 -0700

    zero cache lifetime for fetching datafile from opti cdn - prevents fetching old versions of datafile

--- src/utils/fetch-optimizely-datafile.js
+++ src/utils/fetch-optimizely-datafile.js
@@ -6,7 +6,7 @@ export async function fetchDatafileFromCDN() {
   console.log(`Fetching Optimizely Datafile for SDK Key: ${sdkKey}`);
 
   try {
-    const response = await fetch(`https://cdn.optimizely.com/datafiles/${sdkKey}.json`);
+    const response = await fetch(`https://cdn.optimizely.com/datafiles/${sdkKey}.json`, {next: { revalidate: 0 }});
     console.log(`Optimizely Datafile fetched successfully`);
     const responseJson = await response.json();
     return responseJson;
