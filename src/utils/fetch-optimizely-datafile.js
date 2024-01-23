b684236d6755f55810b0560b76e895be4c6cdd4d
Marshall Serlin <marshallserlin@gmail.com>
2024-01-23 15:50:51 -0800

    code cleanup

--- src/utils/fetch-optimizely-datafile.js
+++ src/utils/fetch-optimizely-datafile.js
@@ -14,18 +14,3 @@ export async function fetchDatafileFromCDN() {
     console.log(error)
   }
 }
-
-export async function getDBDatafile() {
-  const sdkKey = 'KHpaznA1c7t2DgGDPaYj9';
-
-  console.log(`Fetching Optimizely Datafile for SDK Key: ${sdkKey}`);
-
-  try {
-    const response = await fetch(`https://cdn.optimizely.com/datafiles/${sdkKey}.json`, {next: { revalidate: 0 }});
-    console.log(`DB Datafile fetched successfully`);
-    const responseJson = await response.json();
-    return responseJson;
-  } catch (error) {
-    console.log(error)
-  }
-}
